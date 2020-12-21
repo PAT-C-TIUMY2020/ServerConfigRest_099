8b772f841787f2619dfe2539b7809bcfaeb565ae
ARGA APRILIAN PUTRA JAYA <arga.aprilian.ft18@mail.umy.ac.id>
2020-12-21 22:31:50 +0700

    Add project files.

---
+++ ServerConfigRest_20180140099_Arga Aprilian Putra Jaya/Program.cs
@@ -0,0 +1,33 @@
+﻿using ServiceRest_20180140099_Arga_Aprilian_Putra_Jaya;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerConfigRest_20180140099_Arga_Aprilian_Putra_Jaya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server Ready");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
